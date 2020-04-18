ffea6bea2bb4a73e027ac359df205e583f1d2744
AsgerHB <asgerhb@gmail.com>
2020-04-18 22:57:34 +0200

    Update fish graphic script

--- game/Scripts/FishGraphic.gd
+++ game/Scripts/FishGraphic.gd
@@ -2,16 +2,20 @@ extends Node2D
 
 export var animation_speed = 0.2
 
+onready var animation_player:AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_animation()
 	
 func start_animation():
-	var animation_player:AnimationPlayer = $AnimationPlayer
-	animation_player.play("SimpleFishAnimation")
+	var animation = animation_player.get_animation_list()[0]
+	animation_player.play(animation)
 	animation_player.playback_speed = animation_speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func set_animation_speed(speed: float):
+	animation_player.playback_speed = speed
+	
+func get_animation_speed():
+	return animation_player.playback_speed
+
